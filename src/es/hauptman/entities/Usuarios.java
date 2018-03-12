dbd679053da1d8a07289f3fd89ee00241c11873b
Diego Hauptman <diegohauptman@gmail.com>
2018-03-13 00:40:07 +0100

    creates Login JDialog

---
+++ src/es/hauptman/entities/Usuarios.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.hauptman.entities;
+
+/**
+ * 
+ * @author Diego
+ */
+public class Usuarios {
+    private int id;
+    private String login;
+    private String contrasena;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+}
+
